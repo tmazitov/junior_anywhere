8b0cbe590344db42371124e0dd05c0ece516d792
vlrlmn <lomakina.valerie@gmail.com>
2024-11-01 16:29:26 +0400

    added models for user

--- backend/users_app/models.py
+++ backend/users_app/models.py
@@ -4,9 +4,14 @@ from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
 class User(models.Model):
+    userId = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20, blank=False)
     second_name = models.CharField(max_length=35, blank=False)
-    second_name = models.CharField(max_length=35, blank=False)
-    email = models.EmailField(blank=True)
+    email = models.EmailField(blank=False)
     phone = PhoneNumberField(blank = False)
-    salary = models.IntegerField(blank=True)
+    password = models.CharField(max_length=20, blank=False)
+
+class UserApply(models.Model):
+    pass
+class UserResume(models.Model):
+    pass
